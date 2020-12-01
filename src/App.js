8a4bafb850017591f3834f12c11506f8b8f39529
ogneyar <ogneyar-ne@ya.ru>
2020-12-01 12:05:49 +0300

    eeeeeeeeeee

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Foo } from './pages/Foo';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
+
+        <NavLink to="/">Go Home</NavLink>
+        <NavLink to="/Foo">Go to Foo</NavLink>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Меняй <code>src/App.js</code> и сохрани для перезапуска.
         </p>
+        
+        <Switch>
+          <Route component={ Home } path="/" exact />
+          <Route component={ Foo } path="/Foo" />
+        </Switch>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +35,8 @@ function App() {
           Изучай React
         </a>
       </header>
-    </div>
+    </div>    
+    </BrowserRouter> 
   );
 }
 
