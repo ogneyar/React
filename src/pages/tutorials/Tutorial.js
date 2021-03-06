ca7d66f838e40df77c502769d445b7dd41a2259d
ogneyar <ogneyar-ne@ya.ru>
2021-03-07 00:14:40 +0300

    delete test

--- src/pages/tutorials/Tutorial.js
+++ src/pages/tutorials/Tutorial.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Tutorial.css';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import Main from './Main'
-import Test from './Test'
 import transformOnScroll from './transformOnScroll'
 
 
@@ -10,18 +9,15 @@ const Tutorial = () => {
 
     return (
         <div>
-            <div className="tutorial-link">                
+            <div className="tutorial-link">      
+                <NavLink className="NavLink" to="/tutorials/transformOnScroll">transformOnScroll</NavLink>  
+                <NavLink className="NavLink" to="/tutorials/main">Tutorial</NavLink>        
                 <NavLink className="NavLink" to="/">Главная</NavLink>
-                <NavLink className="NavLink" to="/tutorials/main">Tutorial</NavLink>
-                <NavLink className="NavLink" to="/tutorials/test">Test</NavLink>
-                <NavLink className="NavLink" to="/tutorials/transformOnScroll">transformOnScroll</NavLink>
-                            
             </div>
             
             <div id="tutorial">            
                 <Switch>
                     <Route component={ Main } path="/tutorials/main" />
-                    <Route component={ Test } path="/tutorials/test" />
                     <Route component={ transformOnScroll } path="/tutorials/transformOnScroll" />
                 </Switch>
             </div>
