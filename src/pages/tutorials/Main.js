a920b2c300d87bd3734881ea723be2a7f650ef0e
ogneyar <ogneyar-ne@ya.ru>
2021-03-09 19:29:34 +0300

    test event onclick on android

--- src/pages/tutorials/Main.js
+++ src/pages/tutorials/Main.js
@@ -2,10 +2,23 @@ import React from 'react'
 import './Main.css'
 
 const Main = () => {
+    // eslint-disable-next-line
+    function ChangeColor(e) {
+        e.preventDefault()
+        let Element = e.target
+        if (Element.style.color === 'green') Element.style.color = 'red';
+        else Element.style.color = 'green';
+        return false;
+    }
 
     return (
         <div id="Main">
-            <h1 className="h1">This main Tutorial page</h1>
+            <a href="/" onClick={e => ChangeColor(e)}>
+                Изменить цвет
+            </a>
+            <h1 className="h1">
+                This main Tutorial page
+            </h1>
         </div>
     )
 }
